a2a6a016af49ec4e4e0be51899b9c700de11e1de
andrewtpalo <43764590+andrewtpalo@users.noreply.github.com>
2022-04-26 16:21:27 -0400

    Hiding keys/tokens

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 var {TwitterApi} = require('twitter-api-v2');
 var config = require('./config.js');
 
+//keys/tokens hidden for security
 module.exports = {
-    appKey: 'NUpFj2EyzvCyfxMtba3cjrp9f',
-    appSecret: 'CmxBKbe4kqKiD91QQYvxNAs82WxvWmVEwpBegnCWuqcpIZSQB7',
-    accessToken: '1517951645476401152-e0Umfwv3q9uADdk50SqgYzrxHOaRRg',
-    accessSecret: 'XcdoIihA26XCFcFEbNyd46KPtzFd5IRJHMza9Fx97WuCt'
+    appKey: '',
+    appSecret: '',
+    accessToken: '',
+    accessSecret: ''
 }
 
 //Bearer Token: AAAAAAAAAAAAAAAAAAAAAN1GbwEAAAAArrJIOtB%2FXMVa3dO283UaVhh05SI%3DFmop7yyUg5kGOSmplZhqWupEgKpJ1ABbXDZLd9xZKm48gpJcQe
